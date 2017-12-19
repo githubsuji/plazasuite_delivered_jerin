4235e33094d07c3a889e1b7da28d4023f75eb7e4
AF40015 <sujikumar.thankarajan@anthem.com>
2017-12-19 20:10:42 +0530

    updated for jwt

--- springbootapp/src/main/java/com/restfulservice/springbootapp/controller/CORSFilter.java
+++ springbootapp/src/main/java/com/restfulservice/springbootapp/controller/CORSFilter.java
@@ -31,18 +31,18 @@ public class CORSFilter implements Filter {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
         httpResponse.setHeader("Access-Control-Allow-Origin", httpRequest.getHeader("Origin"));
         httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-    	httpResponse.setHeader("Access-Control-Allow-Headers", "X-Auth-Token, Content-Type, responseType, origin, accept, authorization");
+    	httpResponse.setHeader("Access-Control-Allow-Headers", "X-Auth-Token, Content-Type, responseType, Origin, Accept, Authorization");
         httpResponse.setHeader("Access-Control-Expose-Headers", "custom-header1, custom-header2");
     	httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+    	httpResponse.setHeader("Access-Control-Max-Age", "4800");
     	httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     	httpResponse.setHeader("Pragma", "no-cache");
     	httpResponse.setHeader("Expires", "0");
-//    	if (httpRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
-//    		  chain.doFilter(httpRequest, httpResponse);
-//    	    } else {
-//    	    }
-        //httpResponse.setHeader("Access-Control-Max-Age", "4800");
-        chain.doFilter(httpRequest, httpResponse);
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+        	httpResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+        	chain.doFilter(httpRequest, httpResponse);
+        }
 
 	}
 
